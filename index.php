e637618dc6e4c52b76a527eb541187b78a832a71
Gurdeep Singh <gurdeep@1wayit.com>
2018-07-24 17:34:04 +0530

    new changes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use \Kunnu\Dropbox as dbx;
 //$appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
 //$webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-Example/2.0");
 
-//echo    $authorizeUrl = $webAuth->start();  
+//echo    $authorizeUrl = $webAuth->start();
 
 //echo "\n 1. Go to: " . $authorizeUrl . "\n";
 //echo "\n 2. Click \"Allow\" (you might have to log in first).\n";
@@ -15,9 +15,9 @@ use \Kunnu\Dropbox as dbx;
 
 $accessToken = "_gaOMfYC23AAAAAAAAADPcEGme5ToXsEN_hZsYuJUgY";
 
-$dbxApp = new dbx\DropboxApp('3hwuscp97rpc6z7','9gug32qx51a2io1','_gaOMfYC23AAAAAAAAADPntwxxIbwHxBNOIbG2hr2F55eUZaD-vxgyLGVu2S0R5P');
+$dbxApp = new dbx\DropboxApp('suguuqcyxtjo5qb','gmqiwhwcy65v5df','d8O1b7pcvqAAAAAAAAAABviuBjEYXLaV0gFUOWrBz4YkETT1tB_-wPzd3ohcH-Yr');
 $accountInfo = $dbxApp->getClientId();
-echo "<pre>##############################################";print_r($accountInfo);
+//echo "<pre>";print_r($accountInfo);
 
 $dbxClient = new dbx\Dropbox($dbxApp);
 
@@ -34,43 +34,63 @@ $dbxClient = new dbx\Dropbox($dbxApp);
 //fclose($f);
 //echo "<pre>**********************************************"; print_r($result);
 
-
-
 $listFolderContents = $dbxClient->listFolder("/");
+echo "<pre>";print_r($listFolderContents);
+$entries = $listFolderContents->entries;
+echo "<pre>";print_r($entries);
+$newItems = array();
+foreach ($entries as $key => $value)
+{
+	if( $value['.tag'] == 'folder' )
+	{
+		$listFolderContents = $dbxClient->listFolder($value['path_lower']."/");
+		//Fetch Items (Returns an instance of ModelCollection)
+		$items = array();
+		$items = $listFolderContents->getItems();
+		if( !empty($items))
+		{
+			foreach ($items as $key1 => $value1)
+			{
+				echo "<pre>";print_r($value1);
+				$itemm['name'] = $value1->name;
+				$itemm['.tag'] = $value1->tag;
+				$itemm['path_lower'] = $value1->path_lower;
+				$itemm['path_display'] = $value1->path_display;
+				$itemm['id'] = $value1->id;
+				$itemm['client_modified'] = $value1->client_modified;
+				$itemm['server_modified'] = $value1->server_modified;
+				$itemm['rev'] = $value1->rev;
+				$itemm['size'] = $value1->size;
+				$itemm['content_hash'] = $value1->content_hash;
 
-echo "<br>-----------"; echo "<pre>"; print_r($listFolderContents); echo "</pre>";
+
+				$newItems[] = $itemm;
+			}
+		}
+	}
+	else
+	{
+		$newItems[] = $value;
+	}
 
 
-$items = $dbxClient->listFolder("/Standard VAT/");
+}
+echo "<pre>";print_r($newItems);
+die();
+
+$items = $dbxClient->listFolder("/sarbrinder/");
 
 echo "<br>**********"; echo "<pre>"; print_r($items); echo "</pre>"; //die;
 
-$folderMetadata = $dbxClient->getMetadataWithChildren("/");
-print_r($folderMetadata);
+//Fetch Cusrsor for listFolderContinue()
+$cursor = $listFolderContents->getCursor();
 
-echo "<br>/////////"; echo "<pre>"; print_r($items); echo "</pre>"; //die;
+//Paginate through the remaining items
+$listFolderContinue = $dropbox->listFolderContinue($cursor);
 
+$remainingItems = $listFolderContinue->getItems();
 
-
-//echo "<br>**********";
-//$folderMetadata = $dbxClient->get('https://api.dropboxapi.com/2/files/list_revisions');
-//print_r($folderMetadata);
-
-die;
-
-
-//If more items are available
-if ($listFolderContents->hasMoreItems()) {
-    //Fetch Cusrsor for listFolderContinue()
-    $cursor = $listFolderContents->getCursor();
-
-    //Paginate through the remaining items
-    $listFolderContinue = $dropbox->listFolderContinue($cursor);
-
-    $remainingItems = $listFolderContinue->getItems();
-
-    echo "<br>**********"; echo "<pre>"; print_r($remainingItems); echo "</pre>";
-}
+echo "<br>*******-****"; echo "<pre>"; print_r($remainingItems); echo "</pre>";
 
 
 ?>
