862066f9e0839bdecd1e148ca433d7d11a88d3cd
unknown <gurdeepbrar207@gmail.com>
2018-07-25 22:16:49 +0530

    list files and download

--- download.php
+++ download.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('max_execution_time', 600); //300 seconds = 5 minutes
-
-ini_set('max_execution_time', 0); //0=NOLIMIT
+ini_set('max_execution_time', 0); 
 # Include the Dropbox SDK libraries
 require_once "vendor/autoload.php";
 use \Kunnu\Dropbox as dbx;
@@ -9,27 +7,18 @@ use \Kunnu\Dropbox as dbx;
 $filePath = isset( $_REQUEST['path'] ) ? $_REQUEST['path'] : '';
 if( $filePath != '' )
 {
-	$accessToken 		= "_gaOMfYC23AAAAAAAAADPcEGme5ToXsEN_hZsYuJUgY";
-	$dbxApp 	 		= new dbx\DropboxApp('suguuqcyxtjo5qb','gmqiwhwcy65v5df','d8O1b7pcvqAAAAAAAAAABviuBjEYXLaV0gFUOWrBz4YkETT1tB_-wPzd3ohcH-Yr');
-	$accountInfo 		= $dbxApp->getClientId();
-	$dbxClient   		= new dbx\Dropbox($dbxApp);
-
-	//$file = $dbxClient->download("/second folder/first sub folder in second folder/first file.txt", 'first file.txt');
-	$file = $dbxClient->download($filePath, 'first file.txt');
-	//Get File Contents
-	$contents = $file->getContents();
-	//Save File Contents to Disk
-	//file_put_contents(__DIR__ . "/first file2.txt", $contents);
-	file_put_contents(__DIR__ . "/first file2.pdf", $contents);
-	$path = __DIR__ . "/first file2.pdf";
-	$filename = "firstfile2.pdf";
-	header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
-	header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
-	header('Accept-Ranges: bytes');  // Allow support for download resume
-	header('Content-Length: ' . filesize($path));  // File size
-	header('Content-Encoding: none');
-	header('Content-Type: application/pdf');  // Change the mime type if the file is not PDF
-	header('Content-Disposition: attachment; filename=' . $filename);  // Make the browser display the Save As dialog
-	readfile($path);
+	try {
+		$dbxApp 	 		= new dbx\DropboxApp('pkj3wpminfit30i','mgdesun91cmiokv','7oYNdBjOuoAAAAAAAAAEr1uVMlPDy3NNsl74VeI_ohIfPVHe3fWCrxUYF-bEf4qE');
+		$dbxClient   		= new dbx\Dropbox($dbxApp);
+		$result = $dbxClient->getTemporaryLink($filePath);               
+		if( $result->link != '' )
+		{
+			header('Location: '.$result->link);
+		}
+	}
+	//catch exception
+	catch(Exception $e) {
+		echo "<p>".$e->getMessage()."</p>";    
+	}            
 }
 ?>
--- index.php
+++ index.php
@@ -1,45 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Dropbox File List</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+  <link rel="shortcut icon" type="image/png" href="https://raw.githubusercontent.com/jaraco/dropbox-index/master/icons/favicon.ico"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css" />
 </head>
 <body>
+
 <?php
 # Include the Dropbox SDK libraries
 require_once "vendor/autoload.php";
 use \Kunnu\Dropbox as dbx;
+$root  = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
-$accessToken 		= "_gaOMfYC23AAAAAAAAADPcEGme5ToXsEN_hZsYuJUgY";
-$dbxApp 	 		= new dbx\DropboxApp('suguuqcyxtjo5qb','gmqiwhwcy65v5df','d8O1b7pcvqAAAAAAAAAABviuBjEYXLaV0gFUOWrBz4YkETT1tB_-wPzd3ohcH-Yr');
-$accountInfo 		= $dbxApp->getClientId();
-$dbxClient   		= new dbx\Dropbox($dbxApp);
-$listFolderContents = $dbxClient->listFolder("/");
-
-$entries 	= $listFolderContents->entries;
-foreach ($entries as $key => $value)
+$flag  = isset( $_REQUEST['flag'] ) ? $_REQUEST['flag'] : 0;
+ini_set('max_execution_time', 0); 
+if( $flag == 1 )
 {
-	if( $value['.tag'] == 'folder' )
+	$dbxApp 	 		= new dbx\DropboxApp('pkj3wpminfit30i','mgdesun91cmiokv','7oYNdBjOuoAAAAAAAAAEr1uVMlPDy3NNsl74VeI_ohIfPVHe3fWCrxUYF-bEf4qE');
+	$accountInfo 		= $dbxApp->getClientId();
+	$dbxClient   		= new dbx\Dropbox($dbxApp);
+	$listFolderContents = $dbxClient->listFolder("/");
+
+	$entries 	= $listFolderContents->entries;
+	foreach ($entries as $key => $value)
 	{
-		getItems($value['path_lower']);		
-	}
-	else
-	{
-		$_POST[] = $value;
+		if( $value['.tag'] == 'folder' )
+		{
+			getItems($value['path_lower']);		
+		}
+		else
+		{
+			$itemm['name'] 			  = $value1['name'];
+			$itemm['path_lower'] 	  = $value1['path_lower'];
+			$_POST[] = $itemm;
+		}
 	}
 }
 
 function getItems( $path = null )
 {
-	$accessToken = "_gaOMfYC23AAAAAAAAADPcEGme5ToXsEN_hZsYuJUgY";
-
-	$dbxApp = new dbx\DropboxApp('suguuqcyxtjo5qb','gmqiwhwcy65v5df','d8O1b7pcvqAAAAAAAAAABviuBjEYXLaV0gFUOWrBz4YkETT1tB_-wPzd3ohcH-Yr');
+	$dbxApp 	 = new dbx\DropboxApp('pkj3wpminfit30i','mgdesun91cmiokv','7oYNdBjOuoAAAAAAAAAEr1uVMlPDy3NNsl74VeI_ohIfPVHe3fWCrxUYF-bEf4qE');
 	$accountInfo = $dbxApp->getClientId();
-	$dbxClient = new dbx\Dropbox($dbxApp);
+	$dbxClient   = new dbx\Dropbox($dbxApp);
 	if( $path != null )
 	{
 		$listFolderContents = $dbxClient->listFolder($path."/");
@@ -52,15 +60,15 @@ function getItems( $path = null )
 				if( $value1['.tag'] == 'file' )
 				{
 					$itemm['name'] 			  = $value1['name'];
-					$itemm['.tag'] 			  = $value1['.tag'];
 					$itemm['path_lower'] 	  = $value1['path_lower'];
+					/*$itemm['.tag'] 			  = $value1['.tag'];
 					$itemm['path_display']    = $value1['path_display'];
 					$itemm['id'] 			  = $value1['id'];
 					$itemm['client_modified'] = $value1['client_modified'];
 					$itemm['server_modified'] = $value1['server_modified'];
 					$itemm['rev'] 			  = $value1['rev'];
 					$itemm['size'] 			  = $value1['size'];
-					$itemm['content_hash'] 	  = $value1['content_hash'];
+					$itemm['content_hash'] 	  = $value1['content_hash'];*/
 					$_POST[] = $itemm;
 				}
 				else if( $value1['.tag'] == 'folder' )
@@ -68,37 +76,63 @@ function getItems( $path = null )
 					$subitems = getItems($value1['path_lower']);					
 				}
 			}
-		}
-		
+		}			
 	}			
 	else
 		return array();
 	
 }
 
-//echo "<pre>";print_r($_POST);
-
 ?>
 <div class="container">
-  <h2>Drop Box Files</h2>
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>File Name</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-	<?php if( !empty($_POST) ) {
-		foreach( $_POST as $keyy=>$vall ){?>
-		<tr>
-		<td><?php echo $vall['name'];?></td>
-		<td><a target="_blank" href="<?php echo 'download.php?path='.$vall['path_lower'];?>">Click Here To Download</a></td>
-		</tr>
-		<?php } 
-	} ?>
-    </tbody>
-  </table>
+	<?php if( $flag == 0 ){?>
+		<h2></h2>
+		<div class="row">
+			<div class="form-group">
+				<div class="col-md-12 text-center"> 
+					<a href="<?php echo $root.'?flag=1';?>" class="btn btn-primary">List Dropbox Files</a>
+				</div>
+			</div>
+
+		</div>		
+	<?php } else{?>	
+		<h2>Drop Box Files</h2>
+		<table id="example" class="table table-striped table-bordered" style="width:100%">
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php if( !empty($_POST) ) {
+				foreach( $_POST as $keyy=>$vall ){?>
+				<tr>
+					<td><?php echo $vall['name'];?></td>
+					<td><a target="_blank" href="<?php echo 'download.php?path='.$vall['path_lower'];?>">Click Here To Download</a></td>
+				</tr>
+			<?php } 
+			} else{?>
+				<tr>
+					<td colspan="2">No files found.</td>					
+				</tr>
+			<?php } ?>	
+			</tbody>
+		</table>
+		
+		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				//$('#example').DataTable();
+				$('#example').dataTable( {
+				  "pageLength": 25,
+				  "lengthMenu": [[10, 25, 50, 100, 150, -1], [10, 25, 50, 100, 150, "All"]]
+				} );
+			} );
+		</script>
+	<?php } ?>
 </div>
 
 </body>
